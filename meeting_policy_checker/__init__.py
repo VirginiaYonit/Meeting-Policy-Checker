7ea2833ce482bfd94e9872156c41241c11dd486c
Virginia Levy Abulafia <virginiayonit@gmail.com>
2025-08-23 10:33:46 +0200

    Update __init__.py

---
+++ meeting_policy_checker/__init__.py
@@ -0,0 +1,18 @@
+from .agent import check_meeting
+from .schema import (
+    InputPayload,
+    OutputPayload,
+    MeetingType,
+    Strictness,
+    GuardrailTrace,
+    Trace,
+)
+__all__ = [
+    "check_meeting",
+    "InputPayload",
+    "OutputPayload",
+    "MeetingType",
+    "Strictness",
+    "GuardrailTrace",
+    "Trace",
+]
